cbf7b45e7dfb860cb00703a5ebbdc81649e503a6
Nick-Omen <nikita@mqd.me>
2018-04-13 01:07:57 +0600

    Add auth middleware

--- main.go
+++ main.go
@@ -21,8 +21,24 @@ func shareRouter(router *mux.Router)  {
 	tag.SetRoutes(router)
 }
 
+func authMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			next.ServeHTTP(w, r)
+		} else {
+			authenticated := true  // TODO add authentication check
+			if authenticated {
+				next.ServeHTTP(w, r)
+			} else {
+				w.WriteHeader(http.StatusForbidden)
+			}
+		}
+	})
+}
+
 func startProduction() {
 	router := mux.NewRouter()
+	router.Use(authMiddleware)
 	db := storage.Connect("api")
 	db.LogMode("true" == os.Getenv("DEV"))
 	shareConnection(db)
